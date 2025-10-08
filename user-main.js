0c9e62882a7e2216b2f72a3c2d9c9c2949d33dff
Prince rama🤴 <princekumar954684@gmail.com>
2025-10-08 11:39:48 +0530

    Update user-main.js

--- user-main.js
+++ user-main.js
@@ -147,12 +147,13 @@ window.addEventListener('beforeinstallprompt', (e) => {
         // === YAHAN BADLAV KIYA GAYA HAI: Color code direct daal diya gaya hai ===
         installContainer.innerHTML = `
     <div class="dynamic-buttons-wrapper" style="padding-top: 0;">
-        <button id="installAppBtn" class="civil-button btn-glossy" style="background-image: linear-gradient(to top, #218838, #28a745); border: none; border-radius: 12px; width: auto;">
+        <button id="installAppBtn" class="civil-button btn-glossy" style="background-image: linear-gradient(to top, #218838, #28a745); color: white; border: none; border-radius: 12px; width: auto;">
             <i data-feather="download-cloud"></i>
             <b>Install App</b>
         </button>
     </div>
 `;
+
 
         // === BADLAV SAMAPT ===
 
